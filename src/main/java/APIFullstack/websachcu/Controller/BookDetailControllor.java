cded50b6fcd832d00a9e1ed7f30a3d53e801c952
VietAnh <nva.clsp@gmail.com>
2021-05-27 15:13:01 +0700

    update link DetailBook

--- src/main/java/APIFullstack/websachcu/Controller/BookDetailControllor.java
+++ src/main/java/APIFullstack/websachcu/Controller/BookDetailControllor.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping(value = "/detailPage")
@@ -19,31 +16,44 @@ public class BookDetailControllor {
 
     @Autowired
     BookRepository bookRepository;
-
     @Autowired
     UserFormSignedIn userFormSignedIn;
 //    @Autowired
 //    PostBookService postBookService;
 //    @PathVariable("bookid") Integer bookId
-    @GetMapping
-    public String detailPage(Model modelDetailPage){
-        UserFormSignedIn userFormSignedIn2=userFormSignedIn;
-        BookEntity bookDetail = new BookEntity();
-//        BookEntity bookDetail =bookRepository.findByNativeQuery(bookId);
-        modelDetailPage.addAttribute("detailBook",bookDetail);
-        modelDetailPage.addAttribute("userFormSignedIn2",userFormSignedIn2);
-        return "detailPage";
-    }
-////Getmapping co RequestParam
 //    @GetMapping
-//    public String detailPage(Model modelDetailPage,
-//                             @RequestParam("id") Integer bookId){
+//    public String detailPage(Model modelDetailPage){
 //        UserFormSignedIn userFormSignedIn2=userFormSignedIn;
-//
-////        BookEntity bookDetail = new BookEntity();
-//        BookEntity bookDetail =bookRepository.findByNativeQuery(bookId);
+//        BookEntity bookDetail = new BookEntity();
+////        BookEntity bookDetail =bookRepository.findByNativeQuery(bookId);
 //        modelDetailPage.addAttribute("detailBook",bookDetail);
 //        modelDetailPage.addAttribute("userFormSignedIn2",userFormSignedIn2);
 //        return "detailPage";
 //    }
+////Getmapping co RequestParam
+    @GetMapping
+    public String detailPage(Model modelDetailPageAndParam,@RequestParam("id") Integer bookId){
+        UserFormSignedIn userFormSignedIn3=userFormSignedIn;
+        modelDetailPageAndParam.addAttribute("userFormSignedIn2",userFormSignedIn3);
+//        if (bookId == null){
+//            modelDetailPageAndParam.addAttribute("detailBook",new BookEntity());
+//            return "detailPage";
+//        }
+//        BookEntity bookDetail = new BookEntity();
+        BookEntity bookDetail =bookRepository.findByNativeQuery(bookId);
+        modelDetailPageAndParam.addAttribute("detailBook",bookDetail);
+        return "detailPage";
+    }
+
+//    @PostMapping
+//    public String detailPage(Model modelDetailPageAndParam,@RequestParam("id") Integer bookId){
+//        UserFormSignedIn userFormSignedIn3=userFormSignedIn;
+//
+////        BookEntity bookDetail = new BookEntity();
+//        BookEntity bookDetail =bookRepository.findByNativeQuery(bookId);
+//        modelDetailPageAndParam.addAttribute("detailBook",bookDetail);
+//        modelDetailPageAndParam.addAttribute("userFormSignedIn2",userFormSignedIn3);
+//        return "detailPage";
+//    }
+
 }
